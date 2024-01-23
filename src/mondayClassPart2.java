ea31b5895caf2e1dd9f3f317c54b8a681fc81697
Asiya <temirkanovaasiya@gmail.com>
2024-01-23 10:22:36 +0600

    test

--- src/mondayClassPart2.java
+++ src/mondayClassPart2.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class mondayClassPart2 {
+public class mondayClassPart2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Monday part 2");
+    }
 }
