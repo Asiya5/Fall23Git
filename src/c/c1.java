7100fa9986dac6fca5a7af83beb47c74d78c667e
Asiya <temirkanovaasiya@gmail.com>
2024-01-28 23:21:27 +0600

    c1-commit-3

--- src/c/c1.java
+++ src/c/c1.java
@@ -4,4 +4,9 @@ public class c1 {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+
+    public static void sayHello() {
+        System.out.println("sayHi");
+    }
+
 }
